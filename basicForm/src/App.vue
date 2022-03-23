69ecd8bf73cd257194928555733019e3d232d208
Felix Anducho <felix.anducho74@gmail.com>
2022-03-22 21:10:33 -0600

    Add basic form practice

---
+++ basicForm/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <the-form @personal-info="personalInfo"></the-form>
+    {{ username }}
+    {{ email }}
+    {{ twitter }}
+  </div>
+</template>
+
+<script>
+import TheForm from "./components/views/TheForm.vue";
+export default {
+  components: { TheForm },
+  name: "App",
+  data() {
+    return {
+      username: "",
+      email: "",
+      twitter: "",
+    };
+  },
+  methods: {
+    personalInfo(info) {
+      this.username = info.username;
+      this.email = info.email;
+      this.twitter = info.twitter;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ basicForm/src/components/views/TheForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="sendForm">
+    <div>
+      <label for="username">Username</label>
+    </div>
+    <div><input type="text" placeholder="Username" v-model="username" /></div>
+    <div>
+      <label for="email">Email</label>
+    </div>
+    <div><input type="text" placeholder="Email" v-model="email" /></div>
+    <div>
+      <label for="twitter">Twitter Username</label>
+    </div>
+    <div>
+      <input type="text" placeholder="Twitter Username" v-model="twitter" />
+    </div>
+    <div>
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      twitter: "",
+    };
+  },
+  methods: {
+    sendForm() {
+      const newPersonalInfo = {
+        username: this.username,
+        email: this.email,
+        twitter: this.twitter,
+      };
+      this.$emit("personal-info", newPersonalInfo);
+    },
+  },
+};
+</script>
+
+<style></style>
