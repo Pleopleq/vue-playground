b033a20ba8ca1d91f779538d1e06d0d5173e5638
Felix Anducho <felix.anducho74@gmail.com>
2023-02-25 18:40:19 -0600

    create mutations for fetching data

--- small-app/src/App.vue
+++ small-app/src/App.vue
@@ -1,4 +1,16 @@
-<script></script>
+<script>
+export default {
+  methods: {
+    foobar() {
+      this.$store.dispatch("fetchBrawlers");
+    },
+  },
+
+  created() {
+    this.foobar();
+  },
+};
+</script>
 
 <template>
   <h1>Brawlers</h1>
--- small-app/src/store.js
+++ small-app/src/store.js
@@ -8,8 +8,18 @@ const store = createStore({
     },
 
     mutations: {
-        fetchBrawlers(state) {
-            console.log(state)
+        fetchBrawlers() {
+            const API_KEY = import.meta.env.VITE_API_KEY;
+
+            fetch('https://api.brawlstars.com/v1/brawlers', {
+                method: "GET",
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: 'Bearer ' + API_KEY,
+                }
+            })
+                .then(response => response.json())
+                .then(data => console.log(data));
         }
     },
 
