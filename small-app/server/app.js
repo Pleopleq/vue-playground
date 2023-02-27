f44bd14beff83a9a489c31b8b9cd16a66f746874
Felix Anducho <felix.anducho74@gmail.com>
2023-02-27 17:51:42 -0600

    Create endpoint to fetch single record

--- small-app/server/app.js
+++ small-app/server/app.js
@@ -1,25 +1,38 @@
-const express = require("express")
-var cors = require('cors')
-require('dotenv').config()
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get("/brawlers", (req, res) => {
-    fetch('https://api.brawlstars.com/v1/brawlers', {
+    fetch("https://api.brawlstars.com/v1/brawlers", {
         method: "GET",
         headers: {
-            Accept: 'application/json',
-            Authorization: 'Bearer ' + process.env.API_KEY,
-        }
+            Accept: "application/json",
+            Authorization: "Bearer " + process.env.API_KEY,
+        },
     })
-        .then(response => response.json())
-        .then(data => res.send(data));
+        .then((response) => response.json())
+        .then((data) => res.send(data));
+});
 
-})
+app.get("/brawler/:id", (req, res) => {
+    const brawlerId = req.params.id;
+    fetch(`https://api.brawlstars.com/v1/brawlers/${brawlerId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + process.env.API_KEY,
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => res.send(data));
+});
 
 app.listen(4000, () => {
-    console.log("Server up and running")
-})
+    console.log("Server up and running");
+});
